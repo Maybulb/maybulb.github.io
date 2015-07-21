2f11e074ef4a0cdc9e03c7e9f1439d4042f5a253
Josh Trommel <joshtrommel@gmail.com>
2015-07-20 23:40:27 -0700

    I hate git

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bright's site
+:globe_with_meridians: Bright's site. Hey, that rhymes. Neat.
+
+## To-do
+
+### ASAP
+- [x] Fix bug where profile boxes are misaligned on retina iPad.
+- [x] Add favicons
---
+++ js/lfm.js
@@ -0,0 +1,32 @@
+// Courtesy of http://www.samkitson.co.uk/using-json-to-access-last-fm-recently-played-tracks/
+// ♫ JavaScript makes me want to swear like Plan B ♫ – JME
+
+$(document).ready(function() {
+
+  $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=jshtrmml&api_key=67bc224ceb7cea73cf835a56d247d15a&limit=2&format=json&callback=?", function(data) {
+
+      var html = '';
+      var counter = 1;
+      $.each(data.recenttracks.track, function(i, item) {
+          if(counter == 1) {
+              html += item.name + ' by ' + item.artist['#text'];
+          }
+          counter++
+      });
+      $('#np1').append(html);
+  });
+
+  $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=ltrlly&api_key=67bc224ceb7cea73cf835a56d247d15a&limit=2&format=json&callback=?", function(data) {
+
+      var html = '';
+      var counter = 1;
+      $.each(data.recenttracks.track, function(i, item) {
+          if(counter == 1) {
+              html += item.name + ' by ' + item.artist['#text'];
+          }
+          counter++
+      });
+      $('#np2').append(html);
+  });
+
+});
